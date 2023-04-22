5310183c8a5cc615e6c2ac61e3c6f14772995d98
Marykeller9 <marykeller9@gmail.com>
2023-04-22 05:58:27 -0700

    Final update printf

--- _printf.c
+++ _printf.c
@@ -12,13 +12,13 @@ int _printf(const char *format, ...)
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-	char buffer[BUFF_SIZE]
+	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 	va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0', i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
@@ -33,6 +33,7 @@ int _printf(const char *format, ...)
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
--- functions2.c
+++ functions2.c
@@ -14,7 +14,7 @@
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	char extra_c = 0, padd = '';
+	char extra_c = 0, padd = ' ';
 	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2 for '0x' */
 	unsigned long num_addrs;
 	char map_to[] = "0123456789abcdef";
@@ -27,13 +27,13 @@ int print_pointer(va_list types, char buffer[],
 		return (write(1, "nil)", 5));
 
 	buffer[BUFF_SIZE - 1] = '\0';
-	UNSUSED(precision);
+	UNUSED(precision);
 
 	num_addrs = (unsigned long)addrs;
 
 	while (num_addrs > 0)
 	{
-		buffer[ind--] map_to[num_addrs % 16];
+		buffer[ind--] = map_to[num_addrs % 16];
 		num_addrs /= 16;
 		length++;
 	}
@@ -42,7 +42,7 @@ int print_pointer(va_list types, char buffer[],
 	if (flags & F_PLUS)
 		extra_c = '+', length++;
 	else if (flags & F_SPACE)
-		extra_c = '', length++;
+		extra_c = ' ', length++;
 
 	ind++;
 
@@ -124,7 +124,7 @@ int print_reverse(va_list types, char buffer[],
 	}
 	for (i = 0; str[i]; i++)
 		;
-	for (i = i - 1; i >= 0, i--)
+	for (i = i - 1; i >= 0; i--)
 	{
 		char z = str[i];
 
